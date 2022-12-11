83cf2b138b6dc3016f34722c3f85b041ae81c764
Jake Swanson <jakswa@gmail.com>
2022-12-11 19:15:11 +0100

    day3 part2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,21 +1,31 @@
 use advent_of_code_2022::input;
+use std::collections::HashSet;
 
 fn main() {
-    let inp = run(input("3"));
-    println!("part1: {}", inp);
+    let inp = input("3");
+    println!("part1: {}", run(&inp));
+    println!("part2: {}", run2(&inp));
 }
 
-fn run2(inp: String) -> i64 {
-    panic!("finish");
+fn run2(inp: &str) -> i64 {
+    let char_groups = inp.lines().map(|i| i.chars().collect::<HashSet<char>>()).collect::<Vec<HashSet<char>>>();
+    char_groups.chunks(3).map(|chunk| {
+        let sect1 = chunk[0].intersection(&chunk[1]).map(|i| *i).collect::<HashSet<char>>();
+        *sect1.intersection(&chunk[2]).next().unwrap()
+    }).map(priority).sum()
 }
 
-fn run(inp: String) -> i64 {
+fn priority(c: char) -> i64 {
+    match c as u8 >= 'a' as u8 {
+        true => c as i64 - 'a' as i64 + 1,
+        false => c as i64 - 'A' as i64 + 27,
+    }
+}
+
+fn run(inp: &str) -> i64 {
     let priorities = inp.lines().map(|line| {
         line.chars()
-            .map(|c| match c as u8 >= 'a' as u8 {
-                true => c as i64 - 'a' as i64 + 1,
-                false => c as i64 - 'A' as i64 + 27,
-            })
+            .map(priority)
             .collect::<Vec<i64>>()
     });
     let dupes = priorities
@@ -30,7 +40,6 @@ fn run(inp: String) -> i64 {
                 .clone()
         })
         .collect::<Vec<i64>>();
-    println!("derp: {:?}", dupes);
     dupes.iter().sum()
 }
 
@@ -39,7 +48,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day3_works() {
+    fn day3_part1_works() {
         assert_eq!(
             run("vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -47,8 +56,22 @@ PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"
-                .to_string()),
+                ),
             157
+        );
+    }
+
+    #[test]
+    fn day3_part2_works() {
+        assert_eq!(
+            run2("vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"
+                ),
+            70
         );
     }
 }
